4c7a666e3d21827040be00934e1242260982ea4e
Van Nhon <nhonpham13@gmail.com>
2023-07-31 01:14:44 +0700

    Setup env IOS

--- App.tsx
+++ App.tsx
@@ -19,11 +19,10 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
   Header,
   LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Config from 'react-native-config';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -76,18 +75,8 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            <Text>{Config.API_SERVICE} Config server</Text>
           </Section>
           <LearnMoreLinks />
         </View>
